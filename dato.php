509ea6f6beea6b3b3f0b7f76334f975377608a5a
samichi <leonbohorquezisabel@gmail.com>
2017-07-29 23:28:24 -0500

    method post

---
+++ dato.php
@@ -0,0 +1,11 @@
+<?php
+#METDO GET
+#$nombre = $_GET['nombre'];
+#$apellido = $_GET['apellido'];
+#echo "Usuario: " . $nombre . ", Apellido: " . $apellido;
+
+#METODO POST
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+echo "Usuario: " . $nombre . ", Apellido: " . $apellido;
+?>
